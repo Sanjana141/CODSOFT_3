f675332a274449d5c7dc261033d21d5a026ffa78
sanjana <160689416+Sanjana141@users.noreply.github.com>
2024-03-10 12:20:59 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,36 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero."
+    return x / y
+
+def main1():
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    if choice not in ['1', '2', '3', '4']:
+        print("Invalid input")
+        return
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+
+if __name__ == "__main1__":
+    main1()
